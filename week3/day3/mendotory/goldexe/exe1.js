e5e1ea1f30876015703dea9cb3b503b1ab41894e
meryemballout <balloutmaryam366@gmail.com>
2025-05-22 10:30:45 +0100

    challenges

---
+++ week3/day3/mendotory/goldexe/exe1.js
@@ -0,0 +1,5 @@
+function isBlank(str) {
+    return str.trim() === '';
+}
+console.log(isBlank(''));
+console.log(isBlank('abc')); 
---
+++ week3/day3/mendotory/goldexe/exe2.js
@@ -0,0 +1,6 @@
+function abbrevName(name) {
+    let arr = name.split(" ");
+    console.log(arr[0] +" "+ arr[1][0] +".");	
+}
+
+abbrevName("Robin Singh");
